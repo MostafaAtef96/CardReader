08c34b6c84a36f79ce6fb0ef827a0850e939ee54
Mostafa Atef <m.atef96@hotmail.com>
2020-11-22 13:55:49 +0200

    Update clint.py

--- clint.py
+++ clint.py
@@ -1,6 +1,6 @@
 import requests
 
-url = 'http://127.0.0.1:5000/ocr'
+url = 'http://31.220.48.190:8000/ocr'
 files = {'front': open('img300f.png', 'rb'),'back': open('img300b.png', 'rb')}
 result = requests.post(url, files=files)
 
